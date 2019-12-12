48cc3bb20ab341c41f77803b6dee4c807533a433
Paul Carduner <paul@carduner.net>
2019-12-11 18:23:43 -0800

    Always shuffle the deck

--- Assets/DeckBehaviour.cs
+++ Assets/DeckBehaviour.cs
@@ -13,18 +13,26 @@ public class DeckBehaviour : MonoBehaviour
     void Awake()
     {
         int n = 0;
+        List<Card> cards = new List<Card>();
         foreach (var suit in (CardSuit[])Enum.GetValues(typeof(CardSuit)))
         {
             for (int value = 1; value <= 13; value++)
             {
-                CardBehaviour card = Instantiate<CardBehaviour>(cardPrefab);
-                card.transform.parent = transform;
-                card.transform.localPosition = Vector3.zero;
-                card.card = new Card(suit, value);
-                card.gameObject.name = card.card.ToString();
-                card.SetFaceUp(false);
-                card.GetComponent<SpriteRenderer>().sortingOrder = n++;
+                cards.Add(new Card(suit, value));
             }
+        }
+        System.Random random = new System.Random();
+        while (cards.Count > 0)
+        {
+            CardBehaviour card = Instantiate<CardBehaviour>(cardPrefab);
+            card.transform.parent = transform;
+            card.transform.localPosition = Vector3.zero;
+            int cardIndex = random.Next(0, cards.Count);
+            card.card = cards[cardIndex];
+            cards.RemoveAt(cardIndex);
+            card.gameObject.name = card.card.ToString();
+            card.SetFaceUp(false);
+            card.GetComponent<SpriteRenderer>().sortingOrder = n++;
         }
     }
 
