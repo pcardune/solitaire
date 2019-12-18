d4b33fcdc44d8d179161e620add505b434e3fa45
Paul Carduner <paul@carduner.net>
2019-12-18 11:56:43 -0800

    Fix layout issues  for different screen aspect ratios and resolutions

---
+++ Assets/Scripts/MatchScreenWidth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class MatchScreenWidth : MonoBehaviour
+{
+
+    // Set this to the in-world distance between the left & right edges of your scene.
+    public float sceneWidth = 10;
+    public float sceneHeight = 10;
+
+    Camera _camera;
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    // Adjust the camera's height so the desired scene width fits in view
+    // even if the screen/window size changes dynamically.
+    void Update()
+    {
+        float desiredHalfHeight = 0.5f * sceneWidth / Screen.width * Screen.height;
+
+        _camera.orthographicSize = Mathf.Max(desiredHalfHeight, sceneHeight / 2);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(Vector3.zero, new Vector3(sceneWidth, sceneHeight, 0));
+    }
+}
