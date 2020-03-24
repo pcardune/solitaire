69b3cfefb90cd77effead3b8fad3a08c90f56402
Paul Carduner <paul@carduner.net>
2020-03-24 10:06:33 -0700

    Move Deck class to its own file

--- Assets/Scripts/Core/Solitaire.cs
+++ Assets/Scripts/Core/Solitaire.cs
@@ -25,30 +25,6 @@ public class Rank
     public const int ACE = 1;
 }
 
-public class Deck
-{
-    public static List<Card> GetShuffledDeck(int randomSeed)
-    {
-        List<Card> shuffledCards = new List<Card>();
-        List<Card> cards = new List<Card>();
-        foreach (var suit in (Suit[])Enum.GetValues(typeof(Suit)))
-        {
-            for (int value = 1; value <= 13; value++)
-            {
-                cards.Add(new Card(suit, value));
-            }
-        }
-        System.Random random = new System.Random(randomSeed);
-        while (cards.Count > 0)
-        {
-            int cardIndex = random.Next(0, cards.Count);
-            shuffledCards.Add(cards[cardIndex]);
-            cards.RemoveAt(cardIndex);
-        }
-        return shuffledCards;
-    }
-}
-
 public enum PileType
 {
     STOCK,
