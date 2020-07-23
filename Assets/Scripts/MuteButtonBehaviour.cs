308725051febc5e0afaebefc06a8a07933dbbe56
Paul Carduner <paul@carduner.net>
2020-07-23 14:18:17 -0700

    Add a mute and unmute button

---
+++ Assets/Scripts/MuteButtonBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteButtonBehaviour : MonoBehaviour
+{
+    private bool isMuted = false;
+    private float oldVolume = 0;
+
+    public Sprite MutedImage;
+    public Sprite UnmutedImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Image>().sprite = UnmutedImage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Toggle()
+    {
+        isMuted = !isMuted;
+        if (isMuted)
+        {
+            if (AudioListener.volume > 0)
+            {
+                oldVolume = AudioListener.volume;
+                AudioListener.volume = 0;
+                GetComponent<Image>().sprite = MutedImage;
+            }
+        }
+        else
+        {
+            AudioListener.volume = oldVolume;
+            GetComponent<Image>().sprite = UnmutedImage;
+        }
+    }
+}
